50224b4be618a856d0c5ba67b38b54ff51d54722
Chris <christopherngo94@gmail.com>
2018-07-27 02:56:52 -0700

    remove dumby expense data

--- src/app.js
+++ src/app.js
@@ -11,13 +11,6 @@ import "react-dates/lib/css/_datepicker.css"
 
 const store = configureStore();
 
-store.subscribe(() => {
-	const state = store.getState()
-	const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
-	console.log(visibleExpenses)
-})
-
-
 const jsx = (
 	<Provider store={store}>
 		<AppRouter/>
