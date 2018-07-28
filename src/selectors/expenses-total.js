cacf6422ce546c7b5a9e529b635c7bec83baa63a
Chris <christopherngo94@gmail.com>
2018-07-28 02:06:57 -0700

    added some comments to our selectExpensesTotal selector function

--- src/selectors/expenses-total.js
+++ src/selectors/expenses-total.js
@@ -1,5 +1,7 @@
-export default (expenses) => {
-	return expenses
-		.map((expense) => expense.amount)
-		.reduce((sum, value) => sum + value, 0);
+const selectExpensesTotal = (expenses) => {
+	return expenses //return our array of expenses
+		.map((expense) => expense.amount) //map over our array and create a new array of expense amounts
+		.reduce((sum, value) => sum + value, 0); //use reduce to add all values in the array
 }
+
+export default selectExpensesTotal
